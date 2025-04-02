8760b5e6dfc44ec2db61f4d06e3ea67173cecbe7
Francisco Thiago <118403131+fcothiago@users.noreply.github.com>
2025-04-02 09:16:31 -0300

    Reimplemented signaling client using classes

--- public/JS/webrtc/signalingserver.js
+++ public/JS/webrtc/signalingserver.js
@@ -1,6 +1,4 @@
 //TODO:Check variables naming. Maybe it's better to padronize termos like reciver and sender
-const remote_peers = new Map();
-
 async function setup_datachannel(peer,reciver)
 {
 	const datachannel = peer.createDataChannel("datachannel");
@@ -32,7 +30,7 @@ async function create_peer(socket,reciver)
 	return peer;
 }
 
-async function handle_peer_request(socket,reciver)
+async function handle_peer_request(socket,reciver,remote_peers)
 {
 	const peer = await create_peer(socket,reciver);
 	const offer = await setup_offer(peer);
@@ -40,7 +38,7 @@ async function handle_peer_request(socket,reciver)
 	socket.emit("rtc-offer",{offer:offer,reciver:reciver});
 }
 
-async function handle_peer_offer(socket,offer,reciver)
+async function handle_peer_offer(socket,offer,reciver,remote_peers)
 {
 	//TODO: Handle whem reciver already exist in remote_peers
 	const peer = await create_peer(socket,reciver);
@@ -50,50 +48,64 @@ async function handle_peer_offer(socket,offer,reciver)
 	socket.emit("rtc-answer",{reciver:reciver,answer:answer});
 }
 
-async function handle_peer_answer(answer,sender)
+async function handle_peer_answer(answer,sender,remote_peers)
 {
 	//TODO: Handle there is no key sender in remote_peers
 	await remote_peers.get(sender).setRemoteDescription(answer);
 }
 
 
-async function handle_icecandidate(icecandidate,sender)
+async function handle_icecandidate(icecandidate,sender,remote_peers)
 {
 	//TODO: Handle there is no key sender in remote_peers
 	await remote_peers.get(sender).addIceCandidate(icecandidate);
 }
 
-function init_signaling_client(socket,folder_name,folder_pass)
+
+class signalingClient
 {
-	socket.on("joined-folder", (result) => {
-		if(!result)
-		{
-			//TODO:Handle Failed Folder Join
-			console.log(result);
-			return;
-		}
-		socket.emit("peer-request",{folder_name:folder_name});
-	});
-	socket.on("peer-request", async (params) =>{
-		console.log(`Recived peer request from ${params.origin}`);	
-		const reciver = params.origin;
-		await handle_peer_request(socket,reciver);
-	});
-	socket.on("rtc-offer", async (params) =>{
-		console.log(`Recived offer from ${params.origin}`);	
-		const reciver = params.origin;
-		const offer = params.offer;
-		await handle_peer_offer(socket,offer,reciver)
-	});
-	socket.on("rtc-answer", async (params) => {
-		console.log(`Recived answer from ${params.origin}`);	
-		const reciver = params.origin;
-		const answer = params.answer;
-		await handle_peer_answer(answer,reciver);
-	});
-	socket.on("rtc-icecandidate", async (params) => {
-		const sender = params.origin , icecandidate = params.icecandidate;
-		await handle_icecandidate(icecandidate,sender);
-	});
-	socket.emit("join-folder",{folder_name:folder_name,folder_pass:folder_pass});
+	constructor(socket,folder_name,folder_pass)
+	{
+		this.socket = socket;
+		this.folder_name = folder_name;
+		this.folder_pass = folder_pass;
+		this.peers = new Map();
+		this.init_signaling_client();
+	}
+
+	init_signaling_client()
+	{
+		this.socket.on("joined-folder", (result) => {
+			if(!result)
+			{
+				//TODO:Handle Failed Folder Join
+				console.log(result);
+				return;
+			}
+			socket.emit("peer-request",{folder_name:this.folder_name});
+		});
+		this.socket.on("peer-request", async (params) =>{
+			console.log(`Recived peer request from ${params.origin}`);	
+			const reciver = params.origin;
+			await handle_peer_request(socket,reciver,this.peers);
+		});
+		this.socket.on("rtc-offer", async (params) =>{
+			console.log(`Recived offer from ${params.origin}`);	
+			const reciver = params.origin;
+			const offer = params.offer;
+			await handle_peer_offer(socket,offer,reciver,this.peers)
+		});
+		this.socket.on("rtc-answer", async (params) => {
+			console.log(`Recived answer from ${params.origin}`);	
+			const reciver = params.origin;
+			const answer = params.answer;
+			await handle_peer_answer(answer,reciver,this.peers);
+		});
+		this.socket.on("rtc-icecandidate", async (params) => {
+			const sender = params.origin , icecandidate = params.icecandidate;
+			await handle_icecandidate(icecandidate,sender,this.peers);
+		});
+		socket.emit("join-folder",{folder_name:this.folder_name,folder_pass:this.folder_pass});
+	}
+
 }
