294703866cdf6b42a677ce7df83e5e98c34e08ca
Francisco Thiago <118403131+fcothiago@users.noreply.github.com>
2025-04-02 12:52:47 -0300

    Added signalingClient error handling callbacks

--- public/JS/webrtc/signalingserver.js
+++ public/JS/webrtc/signalingserver.js
@@ -71,6 +71,11 @@ class signalingClient
 		this.folder_pass = folder_pass;
 		this.peers = new Map();
 		this.init_signaling_client();
+		this.onJoinfolderError = (result) => {};
+		this.onPeerRequestError = (error) => {};
+		this.onRtcOfferError = (error) => {};
+		this.onRtcAnswerError = (error) => {};	
+		this.onIcecandidateError = (error) => {};	
 	}
 
 	init_signaling_client()
@@ -78,32 +83,47 @@ class signalingClient
 		this.socket.on("joined-folder", (result) => {
 			if(!result)
 			{
-				//TODO:Handle Failed Folder Join
-				console.log(result);
-				return;
+				this.onJoinfolderError(result)
 			}
 			socket.emit("peer-request",{folder_name:this.folder_name});
 		});
 		this.socket.on("peer-request", async (params) =>{
 			console.log(`Recived peer request from ${params.origin}`);	
 			const reciver = params.origin;
-			await handle_peer_request(socket,reciver,this.peers);
+			try{
+				await handle_peer_request(socket,reciver,this.peers);
+			}catch(error){
+				this.onPeerRequestError(error);
+			}
 		});
 		this.socket.on("rtc-offer", async (params) =>{
 			console.log(`Recived offer from ${params.origin}`);	
 			const reciver = params.origin;
 			const offer = params.offer;
-			await handle_peer_offer(socket,offer,reciver,this.peers)
+			try{
+				await handle_peer_offer(socket,offer,reciver,this.peers)
+			}catch(error){
+				this.onRtcOfferError(error);
+			}
 		});
 		this.socket.on("rtc-answer", async (params) => {
 			console.log(`Recived answer from ${params.origin}`);	
 			const reciver = params.origin;
 			const answer = params.answer;
-			await handle_peer_answer(answer,reciver,this.peers);
+			try{
+				await handle_peer_answer(answer,reciver,this.peers);
+			}catch(error){
+				this.onRtcAnswerError(error);	
+			}
+
 		});
 		this.socket.on("rtc-icecandidate", async (params) => {
 			const sender = params.origin , icecandidate = params.icecandidate;
-			await handle_icecandidate(icecandidate,sender,this.peers);
+			try{
+				await handle_icecandidate(icecandidate,sender,this.peers);
+			}catch(error){
+				this.onIcecandidateError(error);	
+			}
 		});
 		socket.emit("join-folder",{folder_name:this.folder_name,folder_pass:this.folder_pass});
 	}
