c489180597aba24a70da7b9944e36979d13597a7
Francisco Thiago <118403131+fcothiago@users.noreply.github.com>
2025-05-16 14:11:44 -0300

    Removed buffer_limit variable in reac_chunk.js to allow user of smaller CHUNK_COUNT values

--- public/JS/webrtc/read_chunk.js
+++ public/JS/webrtc/read_chunk.js
@@ -1,5 +1,3 @@
-const buffer_limit = 100;
-
 function file_to_base64(file)
 {
 	return new Promise((resolve,reject) => 
@@ -22,15 +20,7 @@ self.onmessage = async (msg) =>
 		const slice_end   = ( (i+1)*chunk_size ) <= file.size ? (i+1)*chunk_size : file.size;
 		const slice = file.slice(slice_start,slice_end);
 		const data_b64 = ( await file_to_base64(slice) ).split(',')[1]; //remove "data:*/*;base64," prefix
-		buffer[i] = [data_b64,slice.size];
-		if(Object.keys(buffer).length >= buffer_limit)
-		{
-			self.postMessage({chunks_base64:buffer,file_id:file_id});
-			buffer = {};
-		}
-		if(slice_end > file.size)
-		{
-			break;
-		}
+		buffer[i] = [data_b64,slice.size];	
 	}
+	self.postMessage({chunks_base64:buffer,file_id:file_id});
 }
