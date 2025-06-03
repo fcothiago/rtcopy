f6aab56c6ed2719240675c603dffd9611f26fd02
Francisco Thiago <118403131+fcothiago@users.noreply.github.com>
2025-06-03 15:26:24 -0300

    fixed download progress status problem

--- public/JS/webrtc/downloadmanager.js
+++ public/JS/webrtc/downloadmanager.js
@@ -3,15 +3,21 @@ function gen_download_key(file_id,dc_id)
 	return `${dc_id}${file_id}`;
 }
 
-function base64_to_uint8array(chunks_b64)
+function gen_range(start,end)
+{
+		const range = new Set();
+		for(let i = start; i < start+end ; i++)
+			range.add(i);
+		return range;
+}
+
+function base64_to_uint8array(chunk_b64)
 {
 	const parsed_chunk = atob(chunk_b64);
 	const chunk_length = parsed_chunk.length;
 	const full_chunk = new Uint8Array(chunk_length);
 	for(let i = 0; i < chunk_length ; i++)
-	{
-		full_chunk[i] = chunk.charCodeAt(i);
-	}
+		full_chunk[i] = chunk_b64.charCodeAt(i);
 	return full_chunk;
 }
 
@@ -36,9 +42,10 @@ class downloadManager
 			return;
 		const onsuccess = (id) =>
 		{
-			this.current_downloads.set(id,
+			this.current_downloads.set(key,
 			{
 				file_id : id,
+				next_block_index: this.chunk_count,
 				file_size : file_infos.size,
 				waiting_chunks_indexes : gen_range(0,this.chunk_count),
 				bytes_recived : 0,
@@ -47,6 +54,7 @@ class downloadManager
 			});
 			this.folder.request_datachunks(0,this.chunk_count,file_id,dc_id);
 		};
+		this.filedb.addFile(file_infos,onsuccess,(e) => console.log(`failed to crete file in db`));
 	}
 
 	delete_download(file_id,dc_id)
@@ -67,41 +75,39 @@ class downloadManager
 		if( !download || download.finished || (!download.waiting_chunks_indexes.has(infos.chunk_index)) )
 			return;
 		this.updateDownloadInfos(infos,download);
-		this.addChunkToDB(donwload,infos);
+		this.addChunkToDB(infos,dc_id,download);
 	}
 
 	updateDownloadInfos(infos,download)
 	{
 		download.waiting_chunks_indexes.delete(infos.chunk_index);
 		download.bytes_recived += infos.chunk_size;
-		download.chunks_in_memory.set(infos.chunk_index,infos.chunk_data);
 		download.finished = download.bytes_recived >= download.file_size;
 	}
 
-	addChunkToDB(donwload,infos)
+	addChunkToDB(infos,dc_id,download)
 	{
 		try
-		{
+	{
 			const chunkUInt8 = base64_to_uint8array(infos.chunk_data);
-			onsuccess = (e) =>
+			const onsuccess = (e) =>
 			{
 				if( (download.waiting_chunks_indexes.size == 0) || (download.bytes_recived >= download.file_size))
 					this.process_datachunks(infos,download,dc_id);
+				download.download_update_callback(download);
 			}
-			this.filedb.addChunk(download.file_id,chunkUInt8,infos.chunk_index,onsucces,(e) => console.log('failed to insert chunk'));
+			this.filedb.addChunk(download.file_id,chunkUInt8,infos.chunk_index,onsuccess,(e) => console.log('failed to insert chunk'));
 		}
 		catch(e){console.log(e);}
 	}
 
 	process_datachunks(infos,download,dc_id)
 	{
-		if(!download.finished)
-		{
-			const last_index = [...download.chunks_in_memory.entries()].reduce( (a,b) => a[0] >= b[0] ? a[0] : b[0] );
-			download.waiting_chunks_indexes = gen_range(last_index+1,this.chunk_count);
-			this.folder.request_datachunks(last_index+1,this.chunk_count,infos.file_id,dc_id);	
-		}
-		download.chunks_in_memory.clear();
+		if(download.finished)
+			return;
+		download.waiting_chunks_indexes = gen_range(this.next_block_index,this.chunk_count);
+		this.folder.request_datachunks(this.next_block_index,this.chunk_count,infos.file_id,dc_id);	
+		this.next_block_index += this.chunk_count;
 	}
 
 	handle_missing_chunks()
