1de40e4588c8683895d7f36f6f1ea512cc36bcc9
fcothiago <f.t.s.goncalves@gmail.com>
2025-07-02 13:29:18 -0300

    Added remove and export buttons

--- public/JS/files/retrivesavedfiles.js
+++ public/JS/files/retrivesavedfiles.js
@@ -1,8 +1,29 @@
-function addRemoveButtons(btngroup,file)
+function addExportButton(buttonsgroup,file,exportmanager)
+{
+	const exportbutton = document.createElement('a');
+	exportbutton.href = '#';
+	exportbutton.text = 'export file';
+	if("showSaveFilePicker" in window )
+		exportbutton.onclick = async () => exportToFileStream({
+			fileid:file.id,
+			file_name:file.name,
+			file_type:file.type
+		},exportbutton,exportmanager);
+	else
+		exportbutton.onclick = () => exportToBlobFileURL(file.id,exportbutton,exportmanager);
+	buttonsgroup.appendChild(exportbutton);
+}
+
+function addRemoveButton(btngroup,file,filedb)
 {
 	const rmvbutton = document.createElement('a');
 	rmvbutton.href = '#';
 	rmvbutton.innerHTML = "<img src='/icons/cross-circle-svgrepo-com.svg' class='remove-button'>";
+	rmvbutton.onclick = () => {
+		onsuccess = (fileid) => document.getElementById(`${fileid}`).remove();
+		onerror = () => console.log(`failed to remove file ${file.id}`);
+		filedb.removeFile(file.id,onsuccess,onerror);
+	};
 	btngroup.appendChild(rmvbutton);
 }
 
@@ -16,15 +37,17 @@ function addFileInfos(fileitem,file)
 	fileitem.appendChild(fileinfos);
 }
 
-function addFileToDirectory(file)
+function addFileToDirectory(file,filedb,exportmanager)
 {
 	const directory = document.getElementById('directory');
 	const fileitem = document.createElement('li');
 	const buttonsgroup = document.createElement('span');
+	fileitem.id = `${file.id}`;
 	buttonsgroup.className = "buttons-group";
  	addFileInfos(fileitem,file);
-	addRemoveButtons(buttonsgroup,file);
+	addRemoveButton(buttonsgroup,file,filedb);
 	add_preview_btn(buttonsgroup,file);
+	addExportButton(buttonsgroup,file,exportmanager)
 	fileitem.appendChild(buttonsgroup);
 	directory.appendChild(fileitem);
 }
@@ -34,7 +57,8 @@ function retriveSavedFiles()
 	console.log('retriveSavedFiles()');
 	let intervalId = null;
 	const filedb = new fileDatabase();
-	const onfilerecived = (file) => addFileToDirectory(file);
+	const exportmanager = new exportFileManager();
+	const onfilerecived = (file) => addFileToDirectory(file,filedb,exportmanager)
 	const onerror = () => console.log("failed to request files");
 	const waitdb = () => {
 		try
